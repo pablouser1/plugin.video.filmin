6ef6f11b79bdb143cf35a6c11867fa07edae05ff
Pablo Ferreiro <me@pabloferreiro.es>
2023-12-29 17:42:07 +0100

    pretty print for api tests

--- tools/api_m.example.py
+++ tools/api_m.example.py
@@ -1,10 +1,14 @@
+import json
 import sys
 sys.path.append('..') # Add parent path to searchable list
-
 from resources.lib.api import Api
+
+def prettyPrint(data):
+    print(json.dumps(data, indent=2))
 
 DOMAIN = "es"
 
 api = Api(DOMAIN)
 
 # .. You can keep testing the api here
+prettyPrint(api.collections())
